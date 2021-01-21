6cf39b17479f83de09f3404d177b4c5e5334d5d3
George Gastaldi <gegastaldi@gmail.com>
2021-01-21 11:36:40 -0300

    Improved test

--- src/test/java/io/quarkus/registry/app/services/RegistryServiceTest.java
+++ src/test/java/io/quarkus/registry/app/services/RegistryServiceTest.java
@@ -5,9 +5,11 @@ import javax.ws.rs.core.MediaType;
 import io.quarkus.test.junit.QuarkusTest;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
 
 @QuarkusTest
 class RegistryServiceTest {
@@ -15,9 +17,20 @@ class RegistryServiceTest {
     @Test
     void includeLatestPlatform() {
         given()
-                .formParams("groupId", "io.quarkus", "artifactId", "quarkus-bom")
+                .formParams("groupId", "io.quarkus",
+                        "artifactId", "quarkus-bom",
+                        "version", "1.11.0.Final")
                 .post("/api/registry/platform").then()
                 .statusCode(HttpStatus.SC_OK)
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON);
+                .header(HttpHeaders.CONTENT_TYPE, containsString(MediaType.APPLICATION_JSON));
+
+        given()
+                .formParams("groupId", "io.quarkus",
+                        "artifactId", "quarkus-bom",
+                        "version", "1.11.0.Final")
+                .post("/api/registry/platform").then()
+                .statusCode(HttpStatus.SC_OK)
+                .header(HttpHeaders.CONTENT_TYPE, containsString(MediaType.APPLICATION_JSON));
+
     }
 }
