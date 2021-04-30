0c43d0fc0fc6eaaedd5c3544d4c8befd05015973
George Gastaldi <gegastaldi@gmail.com>
2021-04-30 16:00:25 -0300

    Deleting imagestream

--- deploy-to-openshift.sh
+++ deploy-to-openshift.sh
@@ -2,6 +2,9 @@
 
 # login to the OpenShift cluster before launching this script
 
+# Delete the image stream to prevent manifest errors
+oc delete is ubi-quarkus-native-binary-s2i
+
 mvn clean package -Dquarkus.kubernetes.deploy=true -Dquarkus.native.container-build=true -Dnative
 
 # add kubernetes.io/tls-acme: 'true' to the route to renew the SSL certificate automatically
