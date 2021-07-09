9fa45cefade87b8265056aa31aea11a9b72f1737
George Gastaldi <gegastaldi@gmail.com>
2021-07-09 17:40:07 -0300

    Using RegistriesConfigLocator.getDefaultRegistry()

--- src/main/java/io/quarkus/registry/app/maven/RegistryDescriptorContentProvider.java
+++ src/main/java/io/quarkus/registry/app/maven/RegistryDescriptorContentProvider.java
@@ -2,7 +2,6 @@ package io.quarkus.registry.app.maven;
 
 import java.io.StringWriter;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.ws.rs.core.HttpHeaders;
@@ -12,13 +11,8 @@ import javax.ws.rs.core.UriInfo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.maven.ArtifactCoords;
-import io.quarkus.registry.app.DatabaseRegistryClient;
 import io.quarkus.registry.catalog.json.JsonCatalogMapperHelper;
-import io.quarkus.registry.config.json.JsonRegistryConfig;
-import io.quarkus.registry.config.json.JsonRegistryDescriptorConfig;
-import io.quarkus.registry.config.json.JsonRegistryNonPlatformExtensionsConfig;
-import io.quarkus.registry.config.json.JsonRegistryPlatformsConfig;
-import org.apache.maven.artifact.Artifact;
+import io.quarkus.registry.config.RegistriesConfigLocator;
 
 @Singleton
 public class RegistryDescriptorContentProvider implements ArtifactContentProvider {
@@ -36,21 +30,8 @@ public class RegistryDescriptorContentProvider implements ArtifactContentProvide
 
     @Override
     public Response provide(ArtifactCoords artifact, UriInfo uriInfo) throws Exception {
-        JsonRegistryConfig config = new JsonRegistryConfig();
-        // Add descriptor
-        JsonRegistryDescriptorConfig descriptorConfig = new JsonRegistryDescriptorConfig();
-        descriptorConfig.setArtifact(new ArtifactCoords(MavenConfig.GROUP_ID, MavenConfig.REGISTRY_ARTIFACT_ID, "json",  MavenConfig.VERSION));
-        config.setDescriptor(descriptorConfig);
-        // Add platforms
-        JsonRegistryPlatformsConfig platformsConfig = new JsonRegistryPlatformsConfig();
-        platformsConfig.setArtifact(new ArtifactCoords(MavenConfig.GROUP_ID, MavenConfig.PLATFORM_ARTIFACT_ID, "json", MavenConfig.VERSION));
-        config.setPlatforms(platformsConfig);
-        // Add non-platforms
-        JsonRegistryNonPlatformExtensionsConfig nonPlatformExtensionsConfig = new JsonRegistryNonPlatformExtensionsConfig();
-        nonPlatformExtensionsConfig.setArtifact(new ArtifactCoords(MavenConfig.GROUP_ID, MavenConfig.NON_PLATFORM_EXTENSIONS_ARTIFACT_ID, "json", MavenConfig.VERSION));
-        config.setNonPlatformExtensions(nonPlatformExtensionsConfig);
         StringWriter sw = new StringWriter();
-        JsonCatalogMapperHelper.serialize(objectMapper, config, sw);
+        JsonCatalogMapperHelper.serialize(objectMapper, RegistriesConfigLocator.getDefaultRegistry(), sw);
         String result = sw.toString();
         final String checksumSuffix = ArtifactParser.getChecksumSuffix(uriInfo.getPathSegments(), artifact);
         if (ArtifactParser.SUFFIX_MD5.equals(checksumSuffix)) {
