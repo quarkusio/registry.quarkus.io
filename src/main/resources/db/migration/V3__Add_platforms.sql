dc2e32c3634fc3b8331fcf891fe43c492f9aeed2
George Gastaldi <gegastaldi@gmail.com>
2021-07-21 13:38:00 -0300

    Remove Quarkus 1.x platform support

--- src/main/resources/db/migration/V3__Add_platforms.sql
+++ src/main/resources/db/migration/V3__Add_platforms.sql
@@ -1,2 +1,3 @@
 INSERT INTO platform (platform_key, name, is_default) VALUES ('io.quarkus.platform', 'Quarkus Community Platform', true);
-INSERT INTO platform (platform_key, name, is_default) VALUES ('io.quarkus', 'Quarkus 1.x Community Platform', false);
+--  Adds Productized platform (unused in community deployment)
+INSERT INTO platform (platform_key, name, is_default) VALUES ('com.redhat.quarkus.platform', 'Quarkus Product Platform', false);
