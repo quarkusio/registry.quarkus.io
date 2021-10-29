2d7c78534595080ebad00ed9c6bb2f4f76ebe876
George Gastaldi <gegastaldi@gmail.com>
2021-10-29 13:59:31 -0300

    Do not return RESTEasy codes in 404s

---
+++ src/main/java/io/quarkus/registry/app/resteasy/NotFoundExceptionMapper.java
@@ -0,0 +1,14 @@
+package io.quarkus.registry.app.resteasy;
+
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundException> {
+    @Override
+    public Response toResponse(NotFoundException exception) {
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+}
