5b8f317cd24392c006c3a2c9506898b7a0e248fe
George Gastaldi <gegastaldi@gmail.com>
2022-02-13 23:17:16 -0300

    Introduce repeatable DB migration for default platform

---
+++ src/main/java/db/migration/R__Define_Default_Platform.java
@@ -0,0 +1,42 @@
+package db.migration;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.Optional;
+
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+
+/**
+ * Define the default platform based on the environment variable
+ */
+public class R__Define_Default_Platform extends BaseJavaMigration {
+
+    @Override
+    public void migrate(Context context) throws Exception {
+        Optional<String> defaultPlatform = ConfigProvider.getConfig().getOptionalValue("QUARKUS_DEFAULT_PLATFORM",
+                String.class);
+        if (defaultPlatform.isEmpty()) {
+            // No Default Platform defined, skip
+            return;
+        }
+        Connection connection = context.getConnection();
+        // Set marked platform as default
+        try (PreparedStatement stmt = connection.prepareStatement(
+                "UPDATE platform set is_default = true where platform_key = ?")) {
+            stmt.setString(1, defaultPlatform.get());
+            int rows = stmt.executeUpdate();
+            if (rows != 1) {
+                throw new IllegalArgumentException(
+                        "Platform " + defaultPlatform.get() + " does not exist. Check the QUARKUS_DEFAULT_PLATFORM setting");
+            }
+        }
+        // Set other platforms as false
+        try (PreparedStatement stmt = connection.prepareStatement(
+                "UPDATE platform set is_default = false where platform_key <> ?")) {
+            stmt.setString(1, defaultPlatform.get());
+            stmt.executeUpdate();
+        }
+    }
+}
