669088ebc37dc561ad3900bcf6eda86557663739
Guillaume Smet <guillaume.smet@gmail.com>
2023-01-24 11:55:22 +0100

    Rewrite ExtensionRelease named queries with DISTINCT ON
    Bring queries that take 2 seconds to queries that takes dozens of ms.

--- src/main/java/io/quarkus/registry/app/model/ExtensionRelease.java
+++ src/main/java/io/quarkus/registry/app/model/ExtensionRelease.java
@@ -9,10 +9,13 @@ import java.util.Optional;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityResult;
+import javax.persistence.FieldResult;
 import javax.persistence.ManyToOne;
-import javax.persistence.NamedQuery;
+import javax.persistence.NamedNativeQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
+import javax.persistence.SqlResultSetMapping;
 
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 import org.hibernate.Session;
@@ -23,30 +26,75 @@ import io.quarkiverse.hibernate.types.json.JsonTypes;
 import io.quarkus.registry.app.util.Version;
 
 @Entity
-@NamedQuery(name = "ExtensionRelease.findNonPlatformExtensions", query = """
-            select ext from ExtensionRelease ext
-            where ext.platforms is empty
-            and (ext.versionSortable) = (
-                select max(ext2.versionSortable) from ExtensionRelease ext2
-                where ext2.extension = ext.extension
-                and (
-                       (ext2.quarkusCoreVersionSortable <= :quarkusCoreSortable
-                        and ext2.quarkusCoreVersionSortable < :upperBound
-                        and ext2.quarkusCoreVersionSortable >= :lowerBound)
-                    or ((select erc.compatible
-                         from ExtensionReleaseCompatibility erc
-                           where erc.extensionRelease = ext2 and erc.quarkusCoreVersion = :quarkusCore) is true)
-                    )
-            )
-        """)
-
-@NamedQuery(name = "ExtensionRelease.findLatestExtensions", query = """
-            select ext from ExtensionRelease ext
-            where (ext.versionSortable) = (
-                select max(ext2.versionSortable) from ExtensionRelease ext2
-                where ext2.extension = ext.extension
+@NamedNativeQuery(name = "ExtensionRelease.findNonPlatformExtensions", query = """
+            SELECT DISTINCT ON(extension_release.extension_id)
+                extension_release.id as extension_release_id,
+                extension_release.created_at as extension_release_created_at,
+                extension_release.extension_id,
+                extension_release.metadata,
+                extension_release.quarkus_core_version,
+                extension_release.quarkus_core_version_sortable,
+                extension_release.version,
+                extension_release.version_sortable,
+                extension.created_at as extension_created_at,
+                extension.name,
+                extension.description,
+                extension.artifact_id,
+                extension.group_id
+            FROM
+                extension_release
+                JOIN extension ON extension.id = extension_release.extension_id
+                LEFT JOIN platform_extension ON extension_release.id = platform_extension.extension_release_id
+                LEFT JOIN extension_release_compatibility ON extension_release_compatibility.extension_release_id = extension_release.id AND extension_release_compatibility.quarkus_core_version = :quarkusCore
+            WHERE
+                platform_extension.id IS NULL AND (
+                    (extension_release.quarkus_core_version_sortable <= :quarkusCoreSortable AND
+                       extension_release.quarkus_core_version_sortable < :upperBound AND
+                       extension_release.quarkus_core_version_sortable >= :lowerBound)
+                    OR extension_release_compatibility.id IS NOT NULL
                 )
-        """)
+            ORDER BY extension_release.extension_id, extension_release.version_sortable DESC
+        """, resultSetMapping = "ExtensionRelease.ExtensionReleaseRelease.Mapping")
+@NamedNativeQuery(name = "ExtensionRelease.findLatestExtensions", query = """
+            SELECT DISTINCT ON(extension_release.extension_id)
+                extension_release.id as extension_release_id,
+                extension_release.created_at as extension_release_created_at,
+                extension_release.extension_id,
+                extension_release.metadata,
+                extension_release.quarkus_core_version,
+                extension_release.quarkus_core_version_sortable,
+                extension_release.version,
+                extension_release.version_sortable,
+                extension.created_at as extension_created_at,
+                extension.name,
+                extension.description,
+                extension.artifact_id,
+                extension.group_id
+            FROM
+                extension_release
+                JOIN extension ON extension.id = extension_release.extension_id
+            ORDER BY extension_release.extension_id, extension_release.version_sortable DESC
+        """, resultSetMapping = "ExtensionRelease.ExtensionReleaseRelease.Mapping")
+@SqlResultSetMapping(name = "ExtensionRelease.ExtensionReleaseRelease.Mapping", entities = {
+        @EntityResult(entityClass = ExtensionRelease.class, fields = {
+                @FieldResult(name = "id", column = "extension_release_id"),
+                @FieldResult(name = "createdAt", column = "extension_release_created_at"),
+                @FieldResult(name = "extension", column = "extension_id"),
+                @FieldResult(name = "metadata", column = "metadata"),
+                @FieldResult(name = "quarkusCoreVersion", column = "quarkus_core_version"),
+                @FieldResult(name = "quarkusCoreVersionSortable", column = "quarkus_core_version_sortable"),
+                @FieldResult(name = "version", column = "version"),
+                @FieldResult(name = "versionSortable", column = "version_sortable"),
+        }),
+        @EntityResult(entityClass = Extension.class, fields = {
+                @FieldResult(name = "id", column = "extension_id"),
+                @FieldResult(name = "createdAt", column = "extension_created_at"),
+                @FieldResult(name = "name", column = "name"),
+                @FieldResult(name = "description", column = "description"),
+                @FieldResult(name = "artifactId", column = "artifact_id"),
+                @FieldResult(name = "groupId", column = "group_id")
+        })
+})
 public class ExtensionRelease extends BaseEntity {
 
     @NaturalId
@@ -115,21 +163,26 @@ public class ExtensionRelease extends BaseEntity {
                 .loadOptional();
     }
 
+    @SuppressWarnings("unchecked")
     public static List<ExtensionRelease> findNonPlatformExtensions(String quarkusCore) {
         DefaultArtifactVersion artifactVersion = new DefaultArtifactVersion(quarkusCore);
         String lowerBound = artifactVersion.getMajorVersion() + ".0.0.A";
         String upperBound = (artifactVersion.getMajorVersion() + 1) + ".0.0.A";
-        return getEntityManager().createNamedQuery("ExtensionRelease.findNonPlatformExtensions", ExtensionRelease.class)
+        List<Object[]> results = getEntityManager().createNamedQuery("ExtensionRelease.findNonPlatformExtensions")
                 .setParameter("lowerBound", Version.toSortable(lowerBound))
                 .setParameter("upperBound", Version.toSortable(upperBound))
                 .setParameter("quarkusCore", quarkusCore)
                 .setParameter("quarkusCoreSortable", Version.toSortable(quarkusCore))
                 .getResultList();
+
+        return results.stream().map(r -> (ExtensionRelease) r[0]).toList();
     }
 
+    @SuppressWarnings("unchecked")
     public static List<ExtensionRelease> findLatestExtensions() {
-        return getEntityManager().createNamedQuery("ExtensionRelease.findLatestExtensions", ExtensionRelease.class)
+        List<Object[]> results = getEntityManager().createNamedQuery("ExtensionRelease.findLatestExtensions")
                 .getResultList();
-    }
 
+        return results.stream().map(r -> (ExtensionRelease) r[0]).toList();
+    }
 }
